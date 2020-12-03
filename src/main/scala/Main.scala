adfd4f39df6f4328fd38d17ef21f0b558660e362
Guillaume Pugnet <guillaume.pugnet@avanade.com>
2020-12-03 10:53:00 +0100

    first commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,13 @@
+import org.apache.spark.sql.SparkSession
+
+object Main {
+  def main(args: Array[String]) {
+    val spark = SparkSession
+                  .builder
+                  .appName("Simple Application")
+                  .config("spark.master", "local")
+                  .getOrCreate()
+
+    spark.stop()
+  }
+}
